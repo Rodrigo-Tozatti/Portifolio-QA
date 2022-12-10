0bb01be19119c71d5201445d09eee739a60df10b
Rodrigo <rodrigo.tozatti@hotmail.com>
2022-12-10 03:01:34 -0300

    Alteracoes

--- cypress/e2e/alura/alura-pic-registro.spec.cy.js
+++ cypress/e2e/alura/alura-pic-registro.spec.cy.js
@@ -1,11 +1,13 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+
 describe('Funcionalidade registro alura pic', () => {
 
-  //beforeEach(() => {
-  //  cy.visit('baseUrl')
-  //});
+  beforeEach(() => {
+    cy.visit('https://alura-fotos.herokuapp.com/')
+  });
 
   it('Verificar mensagens de validacao', () => {
-    cy.visit('https://alura-fotos.herokuapp.com/')
     cy.contains('a', 'Register now').click()
     cy.contains('button', 'Register').click()
     cy.contains('small', 'Email is required!').should('be.visible')
@@ -13,26 +15,66 @@ describe('Funcionalidade registro alura pic', () => {
     cy.contains('small', 'Full name is required!').should('be.visible')
     cy.contains('small', 'User name is required!').should('be.visible')
     cy.contains('small', 'Password is required!').should('be.visible')
-  })
+  });
 
   it('Verificar mensagem de e-mail invalido', () => {
-    cy.visit('https://alura-fotos.herokuapp.com/#/home/signup')
+    cy.contains('a', 'Register now').click()
     cy.get('input[formcontrolname="email"]').type('rodrigo')
     cy.contains('button', 'Register').click()
     cy.contains('small', 'Invalid e-mail').should('be.visible')
   });
 
   it('Verificar senha com menos de 8 caracteres', () => {
-    cy.visit('https://alura-fotos.herokuapp.com/#/home/signup')
+    cy.contains('a', 'Register now').click()
     cy.get('input[formcontrolname="password"]').type('123')
     cy.contains('button', 'Register').click()
     cy.contains('small', 'Mininum length is 8').should('be.visible')
   });
 
   it('Verificar user name com letras maiusculas', () => {
-    cy.visit('https://alura-fotos.herokuapp.com/#/home/signup')
+    cy.contains('a', 'Register now').click()
     cy.get('input[formcontrolname="userName"]').type('RODRIGO')
     cy.contains('button', 'Register').click()
     cy.contains('small', 'Must be lower case').should('be.visible')
   });
+
+  it('Login com usuario valido', () => {
+    cy.login('flavio', '123')
+    cy.contains('a.mr-1', 'flavio').should('be.visible')
+  });
+
+  it('Registrando um novo usuario (comando personalizado)', () => {
+    let emailfaker = faker.internet.email()
+    let nomefaker = faker.name.firstName()
+    //let usernamefaker = faker.internet.userName()
+    //let senhafaker = faker.internet.password()
+
+    cy.registro(emailfaker, nomefaker, 'marco-01', '$12345678$')
+
+    cy.contains('button', 'Register').click()
+    cy.contains('button', 'Register').click()
+
+    cy.contains('h4', 'Login').should('be.visible')
+  });
+
+  const usuarios = require ('../../fixtures/example.json')
+  usuarios.forEach(usuario => {
+    it.only('Registrando um novo usuario (massa de dados)', () => {
+      cy.contains('a', 'Register now').click()
+      cy.get('input[formcontrolname="email"]').type(email)
+      cy.get('input[formcontrolname="fullName"]').type(fullname)
+      cy.get('input[formcontrolname="userName"]').type(username)
+      cy.get('input[formcontrolname="password"]').type(passworld)
+  
+      cy.contains('button', 'Register').click()
+      cy.contains('button', 'Register').click()
+  
+      cy.contains('h4', 'Login').should('be.visible')
+    });
+
+  })
+
+  
+
+
 })
